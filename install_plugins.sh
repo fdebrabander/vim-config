71991bae048757d17f501b17b584fd500566da4c
Frank de Brabander <debrabander@gmail.com>
2013-08-29 15:22:49 +0200

    Added git clones for used vim plugins

--- install_plugins.sh
+++ install_plugins.sh
@@ -6,3 +6,10 @@ FETCH="curl -Sso "
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 ${FETCH} ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+cd ~/.vim/bundle
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/kien/rainbow_parentheses.vim.git
+git clone https://github.com/kevinw/pyflakes.git
+git clone https://github.com/kevinw/pyflakes-vim.git
+git clone https://github.com/jbking/vim-pep8.git
