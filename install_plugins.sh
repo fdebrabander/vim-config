73ae8f13ca2325a3bd6dc102bccdab183f4d1dc2
Frank de Brabander <debrabander@gmail.com>
2018-03-15 11:07:08 +0100

    Update install_plugins.sh

--- install_plugins.sh
+++ install_plugins.sh
@@ -8,8 +8,22 @@ ${FETCH} ~/.vim/autoload/pathogen.vim \
     https://github.com/tpope/vim-pathogen/raw/master/autoload/pathogen.vim
 
 cd ~/.vim/bundle
-git clone https://github.com/airblade/vim-gitgutter.git
+
+# Show matching parentheses
 git clone https://github.com/kien/rainbow_parentheses.vim.git
+
+# Python syntax chekers
 git clone https://github.com/kevinw/pyflakes.git
 git clone https://github.com/kevinw/pyflakes-vim.git
 git clone https://github.com/jbking/vim-pep8.git
+
+# Git tools
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/tpope/vim-fugitive.git
+
+# Status line
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline-themes.git
+
+# VIM color theme
+git clone https://github.com/altercation/vim-colors-solarized.git
